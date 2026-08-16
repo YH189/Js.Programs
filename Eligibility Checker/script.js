6805495ad4d253ce94af9aea83f3a5a7cc15a30d
YH <yhrj4874@gmail.com>
2026-08-16 09:00:57 +0530

    Update script.js

--- Eligibility Checker/script.js
+++ Eligibility Checker/script.js
@@ -9,7 +9,7 @@ btnclick.onclick = function () {
   if (age == 18) {
     resultshow.textContent = `You are eligible`;
   } else if (age <= 0) {
-    resultshow.textContent = `This age doesn't exist`;
+    resultshow.textContent = `Enter Positive number`;
   } else if (age < 18) {
     resultshow.textContent = `You must be 18+ to apply`;
   } else if (age <= 95) {
